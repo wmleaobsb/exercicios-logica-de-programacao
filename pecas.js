20cd9ae1da334b42cb4596bbdb0daa119202f3aa
wmleaobsb <wesleydemelloleao@gmail.com>
2023-10-16 16:38:01 -0300

    commit do primeiro exercicio de lógica no VSCODE

---
+++ pecas.js
@@ -0,0 +1,10 @@
+// se a peça possuir peso superior a 100g pode cadastrar
+
+let peso = 101;
+
+if (peso < 100){
+    console.log("A peça precisa pesar no mínimo 100g")
+}else{
+    console.log("A peça possui peso adequado")
+}
+
